3635ead6235036ceaca3f435013e7b3316fd9cad
Sanjeev <sanjeev.ksv@gmail.com>
2025-04-02 14:59:01 +0530

    json page changes

---
+++ src/app/components/FormTwo.tsx
@@ -0,0 +1,12 @@
+'use client'
+import React from 'react'
+export default function FormTwo() {
+  
+  const selector:React_Type=useSelector((state:RootState)=>state.Language.data)
+  
+  return (
+    <div>
+   
+     </div>
+  )
+}
